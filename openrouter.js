93f486eadbe983588a0d01e581208d6f6b11297c
kalharaweragoda <kalharaweragoda@gmail.com>
2026-03-23 12:11:55 +0530

    create function

--- openrouter.js
+++ openrouter.js
@@ -1,18 +1,22 @@
-fetch('https://openrouter.ai/api/v1/chat/completions', {
-  method: 'POST',
-  headers: {
-    Authorization: 'Bearer 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',
-    'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
-    'X-OpenRouter-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    model: 'openai/gpt-5.2',
-    messages: [
-      {
-        role: 'user',
-        content: 'What is the meaning of life?',
-      },
-    ],
-  }),
-});
+function request() {
+    
+  fetch("https://openrouter.ai/api/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Bearer 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",
+      "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
+      "X-OpenRouter-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "openai/gpt-5.2",
+      messages: [
+        {
+          role: "user",
+          content: "What is the meaning of life?",
+        },
+      ],
+    }),
+  });
+}
